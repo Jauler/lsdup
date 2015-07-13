4cf06204224c53a7eae469de7a7beb5ca97c4208
Rytis Karpuska <rytis.karpuska@gmail.com>
2015-07-13 13:27:59 +0300

    Adding dummy main file

---
+++ jauler-lsdup.c
@@ -0,0 +1,17 @@
+/*
+ * Author: Rytis Karpuška
+ * 			rytis.karpuska@gmail.com
+ * 			2015
+ *
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Hello World\n");
+
+	return 0;
+}

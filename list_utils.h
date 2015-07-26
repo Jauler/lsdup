537c5000658a37525482053b62a0be918c9f72f7
Rytis Karpuska <rytis.karpuska@gmail.com>
2015-07-27 00:06:27 +0300

    adding some list utils: data, key, next, foreach

---
+++ list_utils.h
@@ -0,0 +1,26 @@
+/*
+ * Lock free hash container implementation
+ *
+ * Author: Rytis Karpuška
+ *         rytis.karpuska@gmail.com
+ *
+ */
+
+#ifndef __LIST_ITERATION_H
+#define __LIST_ITERATION_H
+
+//NOTE: These macros only valid in multithreaded environment if list structure
+//does not change!!!
+
+//Data acquisition
+#define L_DATA(list)				((list)->data)
+#define L_KEY(list)					((list)->n_key)
+
+//Movement
+#define L_NEXT(list)				((list)->next.ptr.ptr)
+
+//Iteration
+#define L_FOREACH(item, list)		for(item = list; item != NULL; item = L_NEXT(item))
+
+#endif
+

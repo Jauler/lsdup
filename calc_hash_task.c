f0eb77215643fa1d750b0124aba84267c48cf25e
Rytis Karpuška <rytis@elektromotus.lt>
2015-07-27 18:32:46 +0300

    First implementation of hashes calculation. UNTESTED

--- calc_hash_task.c
+++ calc_hash_task.c
@@ -7,15 +7,23 @@
  */
 
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 #include "errno.h"
 #include "thread_pool.h"
 #include "lf_map.h"
 #include "mpmc_lf_queue.h"
 #include "list_utils.h"
+#include "file_desc.h"
+#include "murmur3_hash.h"
 
 #include "calc_hash_task.h"
+
+#define CHT_HASH_CHUNK			1048576
+
 
 struct cht_enq_files_arg {
 	struct map *m;
@@ -26,7 +34,50 @@ struct cht_enq_files_arg {
 //Worker for calculating hash values for files
 void cht_hash_calc_worker(void *_arg)
 {
-	//TODO: implement
+	FILE *f;
+	struct file_desc *fd = _arg;
+	int hashed_size = 0;
+	int curr_size;
+	uint8_t *buff = malloc(CHT_HASH_CHUNK);
+	if(buff == NULL){
+		fprintf(stderr, "Error: %s\n", strerror(ENOMEM));
+		return;
+	}
+
+	//open file
+	f = fopen(fd->filename, "r");
+	if(f == NULL){
+		fprintf(stderr, "Error: %s: %s\n", fd->filename, strerror(errno));
+		free(buff);
+		return;
+	}
+
+	//Read in chunks and calculate hash
+	memset(fd->hash, 0, sizeof(fd->hash));
+	while(hashed_size < fd->size){
+		//Calculate current chunk size
+		curr_size = fd->size - hashed_size;
+		if(curr_size > CHT_HASH_CHUNK)
+			curr_size = CHT_HASH_CHUNK;
+
+		if(fread(buff, curr_size, 1, f) != 1){
+			fprintf(stderr, "Read error\n");
+			goto CLEANUP;
+		}
+
+		//Calculate hash of current chunk
+		murmur3(buff, curr_size, fd->hash);
+
+		//increment hashed size
+		hashed_size += curr_size;
+	}
+
+	//Validate hash
+	fd->hash_valid = 1;
+
+CLEANUP:
+	fclose(f);
+	free(buff);
 	return;
 }
 
@@ -50,6 +101,10 @@ void cht_enq_files_worker(void *_arg)
 		//Get potential matches list
 		matchlist = L_DATA(mi);
 		if(matchlist == NULL)
+			continue;
+
+		//No need for hashing zero-sized files
+		if(L_KEY(mi) == 0)
 			continue;
 
 		//Check if we have enough elements for hashes to be useful
